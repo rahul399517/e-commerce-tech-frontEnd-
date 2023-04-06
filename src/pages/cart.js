f06de197400bcf2037069f137172c212de2fb350
Rahul  Kumar <121877673+rahul399517@users.noreply.github.com>
2023-04-05 23:26:26 -0700

    The code window.location.href = window.location.href will reload the current page
    //window.location.reload();  <- not working
           // window.renderReload();  //in render.com , we use this code insted of window.location.reload();//<- not working 
           window.location.href = window.location.origin <- this will navigte to home page , i want treload same page

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -133,7 +133,7 @@ function Cart() {
       //(a) : this code will refersh the page
       //window.location.reload();
        // window.renderReload();  //in render.com , we use this code insted of window.location.reload();//<- not working 
-       window.location.href = window.location.origin;
+       window.location.href = window.location.href;   //window.location.origin <- this will navigte to home page 
       // (b) : this below code will set the default value if refresh to true , and will stop the page to refresh again
       setRefresh(true);
       //(c) : this below code will scroll the page to top ,after pafe gets refreshed by executing useEffrct code , which declared below
